714eb0987a94b26eae0ae8425e2d22761b68f154
Igor <Igor.Khilkevich@russianpost.ru>
2021-01-16 22:24:42 +0300

    vue-boot

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+<!--    <h1>{{ msg }}</h1>-->
+    <h1>{{ callRestService() }}</h1>
+
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,13 +35,38 @@
   </div>
 </template>
 
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'HelloWorld',-->
+<!--  props: {-->
+<!--    msg: String-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      response: [],
+      errors: []
+    }
+  },
+
+  callRestService() {
+    axios.get(`api/ping`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.response = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
