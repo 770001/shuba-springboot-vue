7c453f4309d60e8d83d7892d39fd864ebffe4d39
Igor <Igor.Khilkevich@russianpost.ru>
2021-01-16 13:19:48 +0300

    Initial commit

---
+++ src/main/java/com/khilkevichigor/shubaspringbootvue/ShubaSpringbootVueApplication.java
@@ -0,0 +1,13 @@
+package com.khilkevichigor.shubaspringbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShubaSpringbootVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShubaSpringbootVueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khilkevichigor/shubaspringbootvue/ShubaSpringbootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khilkevichigor.shubaspringbootvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShubaSpringbootVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
