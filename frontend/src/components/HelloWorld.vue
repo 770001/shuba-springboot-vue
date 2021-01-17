7dfe9a48dcab0cd154f8fb2f201a9593238a8cd3
Igor <Igor.Khilkevich@russianpost.ru>
2021-01-17 23:09:12 +0300

    info

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Иди на вкладку Rest и тыркай на кнопку. Имя пользователя тянется из БД!!!</h1>
+    <h3>{{ msg }}</h3>
 
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
